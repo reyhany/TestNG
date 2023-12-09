ceaa1b6879c289a3c70832dd59faf63460634398
reyhany <breyhanyildiz@gmail.com>
2023-12-09 08:15:15 +0100

    first commit

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+static WebDriver driver;
+    public static WebDriver getDriver(){
+        WebDriverManager.chromedriver().setup();
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver(){
+
+        driver.close();
+    }
+
+    public static void quitDriver(){
+
+        driver.quit();
+    }
+}
